8137b9e023b5748b621e88d78ad626a222ecac7f
Alberto Griggio <griggio@fbk.eu>
2018-01-27 23:15:11 +0100

    added license

--- make_arq.py
+++ make_arq.py
@@ -1,3 +1,21 @@
+#!/usr/bin/env python
+#
+#    make_arq - A tool for generating Sony A7RIII Pixel-Shift ARQ files
+#    Copyright (C) 2018 Alberto Griggio <alberto.griggio@gmail.com>
+#
+#    make_arq is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    make_arq is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 from __future__ import print_function
 import os, sys
 import argparse
